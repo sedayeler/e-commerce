426e3dee378d27b9f4c0ec8eb4aa6eac8b9417b8
Seda Yeler <sedaayeler@gmail.com>
2024-10-22 10:16:59 +0300

    First commit

---
+++ Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Contexts
+{
+    public class ECommerceAPIDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.ConfigurationString);
+        }
+
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Order> orders { get; set; }
+    }
+}
